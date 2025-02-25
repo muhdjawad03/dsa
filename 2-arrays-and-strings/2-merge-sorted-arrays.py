7c1abaa12ef06cf3e71975e32947298eb3ebe693
muhdj <muhdjawad03@gmail.com>
2025-02-25 07:43:22 +0530

    Removed assumption for merge-sorted-arrays and added edge case handling

--- 2-arrays-and-strings/2-merge-sorted-arrays.py
+++ 2-arrays-and-strings/2-merge-sorted-arrays.py
@@ -11,7 +11,15 @@ import copy
 
 # Bruteforce
 def merge_sorted_arrays_bf(lst1: list, lst2: list) -> list:
-    # Assuming the lists are always not empty
+    if not lst1 and not lst2:
+        print("Both input lists are empty")
+        return []
+
+    if not lst1:
+        return lst2  # lst2 is not empty and is already in sorted order
+    if not lst2:
+        return lst1  # lst1 is not empty and is already in sorted order
+
     merged_list = copy.copy(lst1)  # O(n)
     merged_list.extend(lst2)  # O(m)
     merged_list.sort()  # O((n+m) log (n+m)) as python uses time sort which has time complexity of n log n
@@ -20,7 +28,15 @@ def merge_sorted_arrays_bf(lst1: list, lst2: list) -> list:
 
 # Better solution with O(n+m)
 def merge_sorted_arrays(lst1: list, lst2: list) -> list:
-    # Assuming the lists are always not empty
+    if not lst1 and not lst2:
+        print("Both input lists are empty")
+        return []
+
+    if not lst1:
+        return lst2  # lst2 is not empty and is already in sorted order
+    if not lst2:
+        return lst1  # lst1 is not empty and is already in sorted order
+
     list1_length = len(lst1)
     list2_length = len(lst2)
     lp1, lp2 = 0, 0  # left_pointer1 for lst1 and left_pointer2 for lst2
